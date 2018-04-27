3c944dd1b4830e7ef57ddb699af50a7aa06358b3
Adrian Herrera <adrian.herrera02@gmail.com>
2018-04-27 19:23:27 +1000

    deobfuscate: added function inline stage

---
+++ src/main/scala/kr/ac/kaist/safe/deobfuscator/FunctionInliner.scala
@@ -0,0 +1,115 @@
+/**
+ * *****************************************************************************
+ * Copyright (c) 2016-2017, KAIST.
+ * Copyright (c) 2018, DST Group.
+ * All rights reserved.
+ *
+ * Use is subject to license terms.
+ *
+ * This distribution may include materials developed by third parties.
+ * ****************************************************************************
+ */
+
+package kr.ac.kaist.safe.deobfuscator
+
+import kr.ac.kaist.safe.errors.ExcLog
+import kr.ac.kaist.safe.errors.error._
+import kr.ac.kaist.safe.nodes.ast._
+
+/**
+ * Performs function inlining on an AST.
+ *
+ * Only trivially simple functions (i.e. those with a single <tt>return</tt>
+ * statement) are inlined.
+ */
+class FunctionInliner(program: Program) {
+  ////////////////////////////////////////////////////////////////
+  // results
+  ////////////////////////////////////////////////////////////////
+
+  lazy val result: Program = FunctionInlineWalker.walk(program, Map[Id, Option[Expr]]())
+
+  lazy val excLog: ExcLog = new ExcLog
+
+  ////////////////////////////////////////////////////////////////
+  // private global
+  ////////////////////////////////////////////////////////////////
+
+  private type Env = Map[Id, Option[Expr]]
+
+  private object FunctionInlineWalker extends ASTEnvWalker[Env] {
+    /**
+     * Generate a map of inlinable functions.
+     *
+     * A function is inlinable if it only contains a single <tt>return</tt>
+     * statement, which we can trivially inline.
+     *
+     * The map associates the name of an inlinable function with the value
+     * that calls to this function will be replaced with.
+     *
+     * @param funcs A list of functions
+     * @return A map of inlinable functions and the expression that we will
+     *         inline them
+     */
+    private def getInlinableFunctions(funcs: List[Functional]): Env =
+      funcs.foldLeft(Map[Id, Option[Expr]]())((m, func) => func.stmts.body.lastOption match {
+        // Update the map with an inlinable function
+        case Some(Return(_, expr)) => m + (func.name -> expr)
+        case None => m + (func.name -> None)
+        // Anything else is too complex to inline
+        case _ => m
+      })
+
+    override def walk(node: TopLevel, env: Env): TopLevel = node match {
+      case TopLevel(info, fds, vds, stmts) =>
+        // Find functions that we can inline
+        val newEnv = env ++ getInlinableFunctions(fds.map(_.ftn))
+        // Inline functions by walking all of the top-level functions,
+        // variable declarations and statements and replacing each
+        // function call with an inlined version of the function
+        val newFds = fds.map(walk(_, newEnv))
+        val newVds = vds.map(walk(_, newEnv))
+        val newStmts = stmts.map(walk(_, newEnv))
+        // Delete inlined functions by filtering them out of the list of
+        // function definitions
+        val filteredFds = newFds.filterNot(fd => newEnv.keySet.contains(fd.ftn.name))
+        TopLevel(info, filteredFds, newVds, newStmts)
+
+      // Rewalk the node if a change has been made to the AST
+      case _ =>
+        val newNode = super.walk(node, env)
+        if (newNode != node) walk(newNode, env) else newNode
+    }
+
+    override def walk(node: Expr, env: Env): Expr = node match {
+      // A function call. Check if it is inlinable, and inline it as
+      // appropriate.
+      case FunApp(info, vr @ VarRef(_, id), args) =>
+        env.find { case (name, _) => name =~ id } match {
+          // An inlinable function that we can replace with the given
+          // expression (taken from the function's return statement).
+          case Some((_, Some(expr))) => expr
+          // An inlinable, empty function. Just replace the function call with
+          // an empty expression
+          case Some((_, None)) => EmptyExpr(info)
+          // If the function is not inlinable, walk the function argument
+          // expressions (because they might be inlinable function calls) and
+          // return the function call expression (with the new arguments).
+          case None =>
+            val newArgs = args.map(walk(_, env))
+            FunApp(info, vr, newArgs)
+        }
+
+      // Rewalk the node if a change has been made to the AST
+      case _ =>
+        val newNode = super.walk(node, env)
+        if (newNode != node) walk(newNode, env) else newNode
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////
+  // calculate results
+  ////////////////////////////////////////////////////////////////
+
+  (result, excLog)
+}
