5a6ac1c20ee76bc0a6f5ad8ccc1ab1c4fbd93e64
Adrian Herrera <adrian.herrera02@gmail.com>
2018-04-28 15:37:44 +1000

    deobfuscate: run all deobfuscators until steady-state
    WIP - not yet working :(

--- src/main/scala/kr/ac/kaist/safe/phase/Deobfuscate.scala
+++ src/main/scala/kr/ac/kaist/safe/phase/Deobfuscate.scala
@@ -26,41 +26,62 @@ case object Deobfuscate extends PhaseObj[Program, DeobfuscateConfig, Program] {
   val help: String =
     "Deobfuscates JavaScript malware (string decoder, constant folder and propagator)"
 
+  private def deobfuscate(program: Program, excLog: ExcLog): (Program, ExcLog) = {
+    var newProgram = program
+    var newExcLog = excLog
+
+    // Decode strings
+    println("run string decoder")
+    val stringDecoder = new StringDecoder(newProgram)
+    newProgram = stringDecoder.result
+    newExcLog += stringDecoder.excLog
+
+    // Fold constants
+    println("run constant folder")
+    val constantFolder = new ConstantFolder(newProgram)
+    newProgram = constantFolder.result
+    newExcLog += constantFolder.excLog
+
+    // Propagate constants
+    println("run constant propagator")
+    val constantPropagator = new ConstantPropagator(newProgram)
+    newProgram = constantPropagator.result
+    newExcLog += constantPropagator.excLog
+
+    // Inline functions
+    println("run function inliner")
+    val functionInliner = new FunctionInliner(newProgram)
+    newProgram = functionInliner.result
+    newExcLog += functionInliner.excLog
+
+    // Remove dead branches
+    println("run dead branch remover")
+    val deadBranchRemover = new DeadBranchRemover(newProgram)
+    newProgram = deadBranchRemover.result
+    newExcLog += deadBranchRemover.excLog
+
+    // Simplify
+    println("run simplifier")
+    newProgram = NU.SimplifyWalker.walk(newProgram)
+
+    if (newExcLog.hasError) {
+      (newProgram, newExcLog)
+    } else if (newProgram =~ program) {
+      (newProgram, newExcLog)
+    } else {
+      // XXX not yet working
+      //deobfuscate(newProgram, newExcLog)
+      (newProgram, newExcLog)
+    }
+  }
+
   def apply(
     pgm: Program,
     safeConfig: SafeConfig,
     config: DeobfuscateConfig
   ): Try[Program] = {
-    var program = pgm
-    var excLog = new ExcLog
-
-    // decode strings
-    val stringDecoder = new StringDecoder(program)
-    program = stringDecoder.result
-    excLog += stringDecoder.excLog
-
-    // fold constants
-    val constantFolder = new ConstantFolder(program)
-    program = constantFolder.result
-    excLog += constantFolder.excLog
-
-    // Propagate constants
-    val constantPropagator = new ConstantPropagator(program)
-    program = constantPropagator.result
-    excLog += constantPropagator.excLog
-
-    // Inline functions
-    val functionInliner = new FunctionInliner(program)
-    program = functionInliner.result
-    excLog += functionInliner.excLog
-
-    // Remove dead branches
-    val deadBranchRemover = new DeadBranchRemover(program)
-    program = deadBranchRemover.result
-    excLog += deadBranchRemover.excLog
-
-    // Simplify
-    program = NU.SimplifyWalker.walk(program)
+    // Run all of the deobfuscators until we reach a steady-state
+    val (program, excLog) = deobfuscate(pgm, new ExcLog)
 
     // Report errors.
     if (excLog.hasError && !safeConfig.testMode && !safeConfig.silent) {
