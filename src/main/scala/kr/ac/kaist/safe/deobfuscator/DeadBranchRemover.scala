2d83372c8b570d2e5a574467caab2c8d54e3bf96
Adrian Herrera <adrian.herrera02@gmail.com>
2018-05-20 16:57:13 +1000

    dead-branch-remove: remove redundant switch cases

--- src/main/scala/kr/ac/kaist/safe/deobfuscator/DeadBranchRemover.scala
+++ src/main/scala/kr/ac/kaist/safe/deobfuscator/DeadBranchRemover.scala
@@ -15,6 +15,7 @@ package kr.ac.kaist.safe.deobfuscator
 import java.math.BigInteger
 import kr.ac.kaist.safe.errors.ExcLog
 import kr.ac.kaist.safe.nodes.ast._
+import scala.annotation.tailrec
 
 /**
  * Removes dead (i.e. unexecutable) branches in \c if statements when the
@@ -53,6 +54,91 @@ class DeadBranchRemover(program: Program) {
       case _: RegularExpression => true
     }
 
+    /**
+     * Flatten a list of case statements into just their body (a list of
+     * statements), starting from the first case statement up to and including
+     * the first case statement that contains a break statement. This is done
+     * to account for "fall through" cases.
+     *
+     * E.g. If we searched for the condition "10" in the following code
+     * snippet only the body of the 2nd case statement would be returned
+     * (because it contains a break statement):
+     *
+     * \code{.js}
+     * switch (0) {
+     *   case 10:
+     *     break;
+     *   case 0:
+     *     a = 10;
+     *     break;
+     *   case 1:
+     *     break;
+     * }
+     * \endcode
+     *
+     * However, this code snippet would return the 2nd and 3rd cases (because
+     * the 2nd case statement doesn't contain a break and hence "falls
+     * through"):
+     *
+     * \code{.js}
+     * switch (0) {
+     *   case 10:
+     *     break;
+     *   case 0:
+     *     a = 10;
+     *   case 1:
+     *     b = 11;
+     *     break;
+     * }
+     * \endcode
+     *
+     * Return a flattened list of statements. If the list of statements
+     * contains a `break` statement, only include up to (and including) the
+     * `break` (everthign after is redundant).
+     */
+    private def flattenCases(cases: List[Case]): List[Stmt] = {
+      // The case statement that we are interested in might not end with a
+      // break statement (i.e. it "falls through" to the next case statement),
+      // so we need to handle this possibility.
+      //
+      // Flatten the list of case statements up until and including the first
+      // case statement that ends with a break statement. Anything following
+      // the break statement is discarded.
+      @tailrec
+      def doFlatten(cases: List[Case], accum: List[Stmt]): List[Stmt] =
+        cases match {
+          case c :: cs =>
+            val idx = breakIndex(c.body)
+            if (idx == -1) {
+              // No break statement - recurse
+              doFlatten(cs, accum ++ c.body)
+            } else {
+              // Make sure we include the break statement
+              accum ++ c.body.slice(0, idx + 1)
+            }
+          case Nil => accum
+        }
+
+      doFlatten(cases, Nil)
+    }
+
+    /**
+     * Returns the index of a break statement in a list of break statements, or
+     * -1 if no break statement exists.
+     */
+    private def breakIndex(stmts: List[Stmt]): Int = stmts.indexWhere {
+      case _: Break => true
+      case _ => false
+    }
+
+    /**
+     * Returns true if all of the cases contain a literal condition expression.
+     */
+    private def allLiteralConds(cases: List[Case]): Boolean = cases.forall {
+      case Case(_, _: Literal, _) => true
+      case _ => false
+    }
+
     override def walk(node: Stmt): Stmt = node match {
       // If the condition is a literal (i.e. constant), evaluate it and remove
       // one of the branches depending on the result.
@@ -66,6 +152,57 @@ class DeadBranchRemover(program: Program) {
           falseBranch match {
             case Some(stmt) => stmt
             case None => EmptyStmt(info)
+          }
+        }
+
+      // TODO handle back cases
+      case Switch(info, cond: Literal, frontCases, defopt, Nil) if allLiteralConds(frontCases) =>
+        // dropWhile will drop case statements until we find one that matches
+        // the given condition
+        def dropUntilCondFound(cs: List[Case]): List[Case] =
+          cs.dropWhile(c => !(c.cond =~ cond))
+
+        // Check if the given list of statements ends with a break statement.
+        def endsWithBreak(stmts: List[Stmt]): Boolean = stmts.lastOption match {
+          case Some(_: Break) => true
+          case _ => false
+        }
+
+        // Look through the front statements until we find one that matches the
+        // literal condition expression. This will return a list of case
+        // statements that includes (a) the case statement that matches the
+        // given condition and (b) all the following case statements. We need
+        // to include the following case statements in case (a) doesn't end in
+        // a break
+        val newFrontCases = dropUntilCondFound(frontCases)
+        // Flatten the reduced list of case statements, starting at the case
+        // statement that matches the given condition. This process will
+        // iterate and merge the case statements until a break statement is
+        // found.
+        flattenCases(newFrontCases) match {
+          // The flattened list of statements ends with a break statement. This
+          // means we can safely wipe out the rest of the switch statement
+          // (because there is no "fall through") and replace it with the
+          // flattened statements.
+          //
+          // Drop the break statement at the end (it is redundant).
+          case frontStmts if endsWithBreak(frontStmts) =>
+            ABlock(info, frontStmts.init, false)
+          // The flattened list of statements is either empty or doesn't end
+          // with a break statement. In either case, we must look to the
+          // default case.
+          //
+          // If a default case exists, then append it to the flattened front
+          // statement. If there is no default statement, then there is nothing
+          // for the front cases to "fall through" to and we just return the
+          // front cases.
+          case frontStmts => defopt match {
+            case Some(defStmts) if endsWithBreak(defStmts) =>
+              ABlock(info, frontStmts ++ defStmts.init, false)
+            case Some(defStmts) =>
+              ABlock(info, frontStmts ++ defStmts, false)
+            case None =>
+              ABlock(info, frontStmts, false)
           }
         }
 
