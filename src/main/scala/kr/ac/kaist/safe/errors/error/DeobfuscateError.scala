96434fdd250636a45dd5e301993cb831cb520245
Adrian Herrera <adrian.herrera02@gmail.com>
2018-09-25 14:29:29 +1000

    constant-propagation: throw error if loop or try/catch is encountered

--- src/main/scala/kr/ac/kaist/safe/errors/error/DeobfuscateError.scala
+++ src/main/scala/kr/ac/kaist/safe/errors/error/DeobfuscateError.scala
@@ -24,14 +24,10 @@ sealed abstract class DeobfuscateError(msg: String, ast: ASTNode) extends SafeEr
 
 sealed abstract class ConstantPropagationError(msg: String, ast: ASTNode) extends DeobfuscateError(msg, ast)
 
-case class EmptyEnvironmentError(ast: ASTNode) extends ConstantPropagationError({
-  "The environment is empty. There should always be at least one frame - the global scope."
-}, ast)
-
 case class InitializedVariableError(id: Id) extends ConstantPropagationError({
   s"The variable '${id.text}' should not have an initializer - the AST writer should have removed this."
 }, id)
 
-case class UndefinedVariableError(id: Id) extends ConstantPropagationError({
-  s"The variable '${id.text}' has not been defined."
-}, id)
+case class UnsupportedStatementError(stmt: Stmt) extends ConstantPropagationError({
+  s"Constant propagation through ${stmt.getClass.getSimpleName} statements is not yet supported."
+}, stmt)
